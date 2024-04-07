b1b739b0e0937edf262f9cdf1953eaba659bb760
lucas__almeida <lucaslap27@gmail.com>
2024-04-07 14:42:08 -0400

    feat: section videos

---
+++ src/components/SectionVideos.tsx
@@ -0,0 +1,9 @@
+export function SectionVideos() {
+  return (
+    <section className="flex w-full h-[880px]">
+      <div className="p-32">
+        <h1 className="text-white font-bold uppercase">Últimos vídeos</h1>
+      </div>
+    </section>
+  );
+}
